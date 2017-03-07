3cdd4e05309a436bd97fd75cf1764fb01ec6856f
Jociele <jociellesouzab@gmail.com>
2017-03-07 11:14:37 -0300

    Criação das classes: Carta.java e CartaDAO.java

---
+++ Postbud/src/main/java/Carta/Carta.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Carta;
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.*;
+
+/**
+ *
+ * @author aluno
+ */
+@Entity
+@Table (name = "usuario")
+public class Carta implements Serializable {
+   @Id
+   @Column(name = "email")
+   private String email;
+   
+   @Column(name = "nome")
+   private String nome;
+   
+   @Column(name = "sobrenome")
+   private String sobrenome;
+   
+   @Column(name = "senha")
+   private String senha;
+   
+   @Column(name = "data_nascimento")
+   @Temporal(javax.persistence.TemporalType.DATE)
+   private Date data_nascimento;
+   
+/* Getters and Setters */ 
+ 
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+   
+    public String getSobrenome() {
+        return sobrenome;
+    }
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+
+    public Date getData_nascimento() {
+        return data_nascimento;
+    }
+    public void setData_nascimento(Date data_nascimento) {
+        this.data_nascimento = data_nascimento;
+    }   
+
+}
